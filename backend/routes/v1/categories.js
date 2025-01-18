cd1ce3fe794db088042dd2e447bcd58f73d85bc2
Burak Kurt <krtburak@outlook.com>
2025-01-18 14:54:38 +0300

    .

--- backend/routes/v1/categories.js
+++ backend/routes/v1/categories.js
@@ -25,8 +25,6 @@ router.post('/', async (req, res, next) => {
       );
     }
 
-    console.log(body);
-
     let category = new Categories({
       name: body.name,
       is_active: body.is_active,
@@ -35,7 +33,58 @@ router.post('/', async (req, res, next) => {
 
     const data = await category.save();
 
+    res.json(Response.successResponse(data, _enum.HTTP_CODES.CREATED));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.statusCode).json(errorResponse);
+  }
+});
+
+router.patch('/', async (req, res, next) => {
+  let body = req.body;
+  try {
+    if (!body._id) {
+      throw new CustomError(
+        _enum.HTTP_CODES.BAD_REQUEST,
+        'Validation error!',
+        'category id field must be filled'
+      );
+    }
+    let updates = {};
+    if (body.name) updates.name = body.name;
+    if (typeof body.is_active === 'boolean') updates.is_active = body.is_active;
+
+    const data = await Categories.findOneAndUpdate({ _id: body._id }, updates);
+
     res.json(Response.successResponse(data));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.statusCode).json(errorResponse);
+  }
+});
+
+router.delete('/', async (req, res, next) => {
+  let body = req.body;
+  try {
+    if (!body._id) {
+      throw new CustomError(
+        _enum.HTTP_CODES.BAD_REQUEST,
+        'Validation error!',
+        'category id field must be filled'
+      );
+    }
+
+    const data = await Categories.findOneAndDelete({ _id: body._id });
+
+    if (data) {
+      res.json(Response.successResponse(data));
+    } else {
+      throw new CustomError(
+        _enum.HTTP_CODES.GONE,
+        'No data found',
+        'The data to be processed may not be found or may have been previously deleted.'
+      );
+    }
   } catch (error) {
     let errorResponse = Response.errorResponse(error);
     res.status(errorResponse.statusCode).json(errorResponse);
